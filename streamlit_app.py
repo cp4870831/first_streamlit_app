a94cc06e2054a826658af06ba5bcdf8ee06e0959
cp4870831 <123458182+cp4870831@users.noreply.github.com>
2023-01-24 10:00:02 +0000

    Update streamlit_app.py
    formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & rocket smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
