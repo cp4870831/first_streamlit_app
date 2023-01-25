6a838367f1ec03bc98e70206183ad40e08face7d
cp4870831 <123458182+cp4870831@users.noreply.github.com>
2023-01-25 16:58:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    streamlit.dataframe()
-except URLError as e:
+# streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error("Please select a fruit to get information.")
+#  else:
+#    streamlit.dataframe()
+#except URLError as e:
  # streamlit.error()
-
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
-  return fruityvice_normalized
+ #   streamlit.dataframe(fruityvice_normalized)
+  
+  def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 streamlit.header('fruityvice fruit advice!')
 try:
