93447599f79537d6908ed1a62f8f61269b85744e
cp4870831 <123458182+cp4870831@users.noreply.github.com>
2023-01-25 17:01:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,17 +54,17 @@ try:
 streamlit.stop()
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("the fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+##streamlit.header("the fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 
-fruit_choices = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding: ', fruit_choices)
+#fruit_choices = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.write('Thanks for adding: ', fruit_choices)
 #import requests
-add_my_fruit_ = requests.get("https://fruityvice.com/api/fruit/" + fruit_choices)
+#add_my_fruit_ = requests.get("https://fruityvice.com/api/fruit/" + fruit_choices)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
